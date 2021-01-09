5e441bbcd6b86041ca8b70650c228e4942fb063f
liutao20 <liutao20@lenovo.com>
2021-01-09 15:16:11 +0800

    del two

--- two/src/main/java/com/liutao/common/AccessConstants.java
+++
@@ -1,9 +0,0 @@
-package com.liutao.common;
-
-/**
- * Created by zhouhh2 on 2016/12/31.
- */
-public class AccessConstants {
-    public static final String ACCESS_PREFIX = "access";
-    public static final String ACCESS_HEADER = "token";
-}
