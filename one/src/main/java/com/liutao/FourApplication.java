19b5772e14ae3d72a54f580dc389dd5691ec1cf9
liutao20 <liutao20@lenovo.com>
2021-01-09 15:33:50 +0800

    del mybatis

--- one/src/main/java/com/liutao/FourApplication.java
+++ one/src/main/java/com/liutao/FourApplication.java
@@ -1,12 +1,9 @@
 package com.liutao;
 
-import com.liutao.conf.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({EnvConfig.class})
 public class FourApplication {
 
 	public static void main(String[] args) {
